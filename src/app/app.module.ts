4bd342ff70d45ea0170e1804a7443596181167d0
Doğukan <dogukanucak94@gmail.com>
2021-06-05 00:36:23 +0300

    ADD : Badge Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BadgeComponent } from "./components/badge/badge.component";
 import { ButtonComponent } from "./components/button/button.component";
 
+const UI_COMPONENTS = [ButtonComponent, BadgeComponent];
+
 @NgModule({
-	declarations: [AppComponent, ButtonComponent],
+	declarations: [AppComponent, ...UI_COMPONENTS],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
