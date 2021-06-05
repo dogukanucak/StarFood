f840a4f78d3e51b89d9dfdf73969f3d74bc2b435
Doğukan <dogukanucak94@gmail.com>
2021-06-05 21:32:05 +0300

    REFACTOR : App Component and Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AcceptedOrderPageComponent } from "./pages/acceptedorderpage/acceptedorder.page.component";
+import { CreateOrderPageComponent } from "./pages/createorderpage/createorder.page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "create", component: CreateOrderPageComponent },
+	{ path: "accepted", component: AcceptedOrderPageComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { BrowserModule } from "@angular/platform-browser";
+import { environment } from "src/environments/environment";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BadgeComponent } from "./components/badge/badge.component";
 import { ButtonComponent } from "./components/button/button.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { AcceptedOrderPageComponent } from "./pages/acceptedorderpage/acceptedorder.page.component";
+import { CreateOrderPageComponent } from "./pages/createorderpage/createorder.page.component";
+import { ActionMenuService } from "./services/menu/actionmenu.service";
+import { OrderMenuService } from "./services/menu/ordermenu.service";
 
-const UI_COMPONENTS = [ButtonComponent, BadgeComponent];
+const UI_COMPONENTS = [ButtonComponent, BadgeComponent, MenuComponent, HeaderComponent];
+const PAGES = [CreateOrderPageComponent, AcceptedOrderPageComponent];
 
 @NgModule({
-	declarations: [AppComponent, ...UI_COMPONENTS],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
+	declarations: [AppComponent, ...UI_COMPONENTS, ...PAGES],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule,
+	],
+	providers: [OrderMenuService, ActionMenuService],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
