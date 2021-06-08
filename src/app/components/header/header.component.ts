bd564c8c845e1c20c9d5f32132d80f9baff5ec8e
Doğukan <dogukanucak94@gmail.com>
2021-06-09 01:44:32 +0300

    FIX : Subscription Unsubscribe

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from "@angular/router";
 import { BehaviorSubject, Subscription } from "rxjs";
 
@@ -7,7 +7,7 @@ import { BehaviorSubject, Subscription } from "rxjs";
 	templateUrl: "./header.component.html",
 	styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 	private routeEventSubscription!: Subscription;
 	readonly currentRouteTextSubject = new BehaviorSubject("");
 	constructor(private router: Router) {}
@@ -19,6 +19,10 @@ export class HeaderComponent implements OnInit {
 				this.currentRouteTextSubject.next(routeText);
 			}
 		});
+	}
+
+	ngOnDestroy(): void {
+		this.routeEventSubscription.unsubscribe();
 	}
 
 	private getRouteTextByURL(url: string): string {
